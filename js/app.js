3aee06bd7c78fbac5a58a12537e23dc3335c85ce
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2016-10-25 20:39:27 -0400

    Start to play

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 // Enemies our player must avoid
 var Enemy = function() {
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    // REMEMBER TO UNCOMMENT function updateEntities(dt)
     this.sprite = 'images/enemy-bug.png';
+    var Num = function(){this.value = Math.floor(Math.random() *  (500 - 20))}
+    var newNum = new Num()
+    this.style.left = `${newNum.value}px`
+
+    allEnemies.push(this);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    var Num = function(){this.value = Math.floor(Math.random() *  (500 - 20))}
+    var newNum = new Num()
+    this.style.left = `${newNum.value}px`
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -24,11 +34,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.sprite = 'images/char-horn-girl.png';
+    this.style.left = '250px'
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var player = Player
+var allEnemies = []
 
 
 
