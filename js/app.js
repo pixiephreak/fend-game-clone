f76153e0ba9f305b9758e5d5ff79dcc987820ebd
Jane Claire <pixiephreak@Janes-MBP.home>
2016-11-15 13:23:37 -0500

    Refactor, comment code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 // Enemies our player must avoid
 //TO-DO: HOW CAN I TAKE THeSe COUNTs OUT OF THE GLOBAL SCOPE
-enemyCount = 2
-score = 0
+var enemyCount = 2;
+var score = 0;
 
+//Define character characteristics with superclass
 var Character = function(sprite, width, height){
     this.sprite = sprite;
     this.width = width;
     this.height = height;
+};
 
-
-}
-
+//draw Character on screen
 Character.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 };
 
-
+//create enemies subclass
 var Enemy = function(x,y,speed) {
-    Character.call(this, 'images/enemy-bug.png', 100, 100)
+    Character.call(this, 'images/enemy-bug.png', 100, 100);
     this.x = x;
     this.y = y;
     this.speed=speed;
@@ -26,6 +26,7 @@ var Enemy = function(x,y,speed) {
 
 };
 
+//link Enemy class to character prototype chain
 Enemy.prototype= Object.create(Character.prototype);
 Enemy.prototype.constructor = Enemy;
 
@@ -33,7 +34,6 @@ Enemy.prototype.constructor = Enemy;
 // Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // console.log('prototypedEnemy')
     // TO-DO: CHANGE ENEMY PATH USING "BOUNCONG BALL" PATTERN? *DOESN'T WORK*
     // var x=100;
     // var y=200;
@@ -44,26 +44,20 @@ Enemy.prototype.update = function(dt) {
     // this.x-=dx + this.speed * dt;;
     // this.y-=dy;
 
-         // multiply any movement by the dt parameter
-        // to ensure the game runs at the same speed for
-        // all computers.
+    // multiply any movement by the dt parameter
+    // to ensure the game runs at the same speed for
+    // all computers.
 
-        this.x = this.x + this.speed * dt;
-        // Have enemies re-render at x = 0 when the reach the right edge
-        if(this.x > 500){
-            this.x = -100;
-            this.y = enemyPlaceVal(50, 250);
-            this.speed = this.speed * 1.1;
-        }
+    this.x = this.x + this.speed * dt;
+    // Have enemies re-render at x = 0 when the reach the right edge
+    if(this.x > 500){
+        this.x = -100;
+        this.y = enemyPlaceVal(50, 250);
+        this.speed = this.speed * 1.1;
+    }
 
 };
 
-// // Draw the enemy on the screen.
-// Enemy.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-
-// };
 
 // check for collision of player/enemy
 Enemy.prototype.checkCollisions = function(other){
@@ -77,27 +71,30 @@ Enemy.prototype.checkCollisions = function(other){
            thisEnemy.y < thisPlayer.y+ 60 &&
            60 + thisEnemy.y > thisPlayer.y) {
             console.log('collision detected!');
-            thisPlayer.x = playerStartX
-            thisPlayer.y = playerStartY
+            thisPlayer.x = playerStartX;
+            thisPlayer.y = playerStartY;
             //empty and recall allEnemies to reset speed incrementation
             allEnemies.length = 0;
             makeEnemies(enemyCount);
-            score = score -2
-            console.log(score)
-            $('#score').html("Score:" + (score) )
+            score = score -2;
+            //Write score to #score div
+            $('#score').html("Score:" + (score) );
         }
     }
 };
 
 
-// The Player class defines player's img and location
+// The Player subclass defines player's img by Character class and location as parameters
 var Player = function(x,y){
-    Character.call(this, 'images/char-horn-girl.png' , 100, 100)
+    Character.call(this, 'images/char-horn-girl.png' , 100, 100);
     this.x = x;
     this.y = y;
 
 
 };
+
+Player.prototype= Object.create(Character.prototype);
+Player.prototype.constructor = Enemy;
 
 //Update player's location on screen according to keydown events. End game when player reaches water and restart game.
 Player.prototype.update = function(dt) {
@@ -107,8 +104,8 @@ Player.prototype.update = function(dt) {
     window.addEventListener('keydown', Player.prototype.handleInput);
     checkScore();
     if (player.y < 50){
-            this.x = playerStartX
-            this.y = playerStartY
+            this.x = playerStartX;
+            this.y = playerStartY;
             winRound();
         }
 };
@@ -145,26 +142,17 @@ Player.prototype.handleInput = function(key){
   default:
     console.log('key other than up, down, left, right arrow pressed');
 
-};
-    // Move player's location on screen, location depending on keydown event
-    // if (key === 'up' && this.y > 10){this.y= this.y - 10; }
-    // if (key === 'down' && this.y < 400){this.y = this.y + 10;}
-    // if (key === 'left' && this.x > 0){this.x = this.x - 10;}
-    // if (key === 'right' && this.x < 400){
-    //     this.x = movePlayerRight(this.x);}
+}
+//TO-DO: why does switch always call default case?
 
 };
-
-
-
-
 
 // Instantiate objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-playerStartX = 207;
-playerStartY = 325;
+var playerStartX = 207;
+var playerStartY = 325;
 var allEnemies = [];
 var player = new Player(playerStartX,playerStartY);
 
@@ -172,7 +160,7 @@ function makeEnemies(num){
         for(let i=0; i<num; i++){
             var newEnemy = new Enemy(-100, enemyPlaceVal(50, 250), enemySpeedVal(85) );
             allEnemies.push(newEnemy);
-            console.log("enemiesFunc")
+            console.log("enemiesFunc");
         }
 
     }
@@ -214,49 +202,38 @@ function enemyPlaceVal(min, max){
     return Math.random() * (max - min) + min;
 }
 
-// function movePlayerLeft(){
-//     this.x = this.x -= 10;
-
-// }
-
-// Play with using a funtion to deterimine player movement. Not concise.
+// Play with using a function to deterimine player movement. Not concise.
 function movePlayerRight(currentX){
     return currentX  += 10;
 }
 
-//Remove enemies from screen and alert user that game is over.
-function endGame(){
-
-
-}
-
-//Alert user when she has won.
+//Alert user when s/he has won.
 function winRound(){
-        enemyCount += 1
+        //increase enemies count by 1 and revert speed to randomized starting val by emptying and recreating the enemies array
+        enemyCount += 1;
         allEnemies.length = 0;
         makeEnemies(enemyCount);
-        console.log ('won', "new enemy count:" + enemyCount)
+        console.log ('won:', "new enemy count:" + enemyCount);
+        //increment score
         score = score +2;
-        $('#score').html("Score:" + score )
-        //call a function that replaces player and creates new enemies.
-        //call makeEnemeies within here and add an enemy or two
-        //create a score div  replace innterHTML
+         //create a score div  replace innterHTML
+        $('#score').html("Score:" + score );
+
+
 
 }
 
 function checkScore(){
     if (enemyCount > 6 && score > 0){
-        window.alert('You Win! Ready to beat your own score?')
+        window.alert('You Win! Ready to beat your own score?');
         document.location.reload(true);
     }if (score < - 8){
-        window.alert('Sorry, Try Again.')
+        window.alert('Sorry, Try Again.');
         document.location.reload(true);
     }
 
 }
 
-// function start(){
 
-// }
 
 
